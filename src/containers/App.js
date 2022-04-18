de21cd7a93acb17a9c2ddcf3a811b44c3ccf8dbf
MoisesTR <moisestrigueros@hotmail.com>
2022-04-17 21:51:24 -0600

    feat: add error boundary

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import Scroll from "../components/Scroll";
+import ErrorBoundry from "../components/ErrorBoundry";
 import "./App.css"
 
 class App extends React.Component {
@@ -14,12 +15,11 @@ class App extends React.Component {
     }
 
     componentDidMount() {
-        console.log(`Did Mount`)
         fetch('https://jsonplaceholder.typicode.com/users')
         .then( response => response.json())
         .then( users => {
             this.setState({ robots: users });
-        })
+        });
     }
 
     onSearchChange = (event) => {
@@ -43,7 +43,9 @@ class App extends React.Component {
                 <h1 className="f1">RoboFriends</h1>
                 <SearchBox searchChange={this.onSearchChange} />
                 <Scroll>
-                    <CardList robots={filteredRobots}></CardList>
+                    <ErrorBoundry>
+                        <CardList robots={filteredRobots}/>
+                    </ErrorBoundry>
                 </Scroll>
             </div>
         );
